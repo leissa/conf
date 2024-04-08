e4e276ee38a81c99dfd704ab2c367e462217e273
Roland Leißa <leissa@uni-mannheim.de>
2024-04-08 04:02:26 +0200

    missing files

---
+++ lua/plugins/nvim-gdb.lua
@@ -0,0 +1,30 @@
+return {
+    'sakhnik/nvim-gdb',
+    ft = {'c', 'cpp'},
+    lazy = false,
+    config = function()
+        local NvimGdb = require'nvimgdb'
+        NvimGdb.setup()
+
+        vim.g.nvimgdb_config = {
+            set_tkeymaps             = function()
+                NvimGdb.here.keymaps:set_t()
+                local buf  = vim.api.nvim_get_current_buf()
+                local opts = { nowait = true }
+                vim.api.nvim_buf_set_keymap(buf, 'n', 'r', '<cmd>GdbRun<cr>',       opts)
+                vim.api.nvim_buf_set_keymap(buf, 'n', 's', '<cmd>GdbStep<cr>',      opts)
+                vim.api.nvim_buf_set_keymap(buf, 'n', 'c', '<cmd>GdbContinue<cr>',  opts)
+                vim.api.nvim_buf_set_keymap(buf, 'n', 'x', '<cmd>GdbNext<cr>',      opts)
+                vim.api.nvim_buf_set_keymap(buf, 'n', 'l', '<cmd>GdbUntil<cr>',     opts)
+                vim.api.nvim_buf_set_keymap(buf, 'n', 'f', '<cmd>GdbFinish<cr>',    opts)
+                vim.api.nvim_buf_set_keymap(buf, 'n', 'u', '<cmd>GdbFrameUp<cr>',   opts)
+                vim.api.nvim_buf_set_keymap(buf, 'n', 'd', '<cmd>GdbFrameDown<cr>', opts)
+                vim.api.nvim_buf_set_keymap(buf, 'n', '<c-p>', '<c-w><c-w><c-p>',   opts)
+            end,
+        }
+    end,
+    keys = {
+        {'<leader>bb',  '<cmd>GdbBreakpointToggle<cr>', desc = "Toggle Breakpoint"},
+        {'<leader>gdb', ':GdbStart gdb -q --args ', desc = "Start GDB"},
+    }
+}
