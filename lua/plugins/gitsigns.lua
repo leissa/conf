406baf28b770b986e9a55bff1f6edc828d160560
Roland Leißa <leissa@uni-mannheim.de>
2024-04-09 19:41:19 +0200

    missing files

---
+++ lua/plugins/gitsigns.lua
@@ -0,0 +1,9 @@
+return {
+    'lewis6991/gitsigns.nvim',
+    opts = {
+        auto_attach = false,
+    },
+    -- config = function(_, opts)
+    -- require('gitsigns').setup(opts)
+    -- end
+}
