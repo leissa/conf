58818446213dfe3fdfded09e21adcc83a07b04c1
Roland Leißa <leissa@uni-mannheim.de>
2025-01-10 21:59:36 +0100

    tuning colorscheme

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,29 @@
+return {
+	{
+		"folke/tokyonight.nvim",
+		opts = {
+			transparent = true,
+			styles = {
+				sidebars = "transparent",
+				floats = "transparent",
+			},
+		},
+	},
+}
+-- return {
+-- 	{
+-- 		"catppuccin/nvim",
+-- 		name = "catppuccin",
+-- 		priority = 1000,
+-- 		opts = {
+-- 			flavor = "moccha",
+-- 			transparent_background = true,
+-- 		},
+-- 	},
+-- 	{
+-- 		"LazyVim/LazyVim",
+-- 		opts = {
+-- 			colorscheme = "catppuccin",
+-- 		},
+-- 	},
+-- }
